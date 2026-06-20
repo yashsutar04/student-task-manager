9e2fd939cca9eed5fff9d49e4d23e9cb148979c0
yashsutar04 <yashsutar477@gmail.com>
2026-06-20 12:32:24 +0530

    Updated requirements.txt and db_config.py

--- db_config.py
+++ db_config.py
@@ -3,10 +3,11 @@ import mysql.connector
 def get_database_connection():
 
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="Yash@2005",
-        database="student_task_manager"
+        host="gateway01.ap-southeast-1.prod.aws.tidbcloud.com",
+        user="3maPQqTGRWhZbZ2.root",
+        password="kkKiUeN2FNo5qCyH",
+        database="student_task_manager",
+        port=4000
     )
 
     return connection
